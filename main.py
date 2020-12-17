8f49b39a07c05905770694507a75b081be10aac1
hnandan6 <hnandan6@gatech.edu>
2020-12-17 03:32:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+def add(number1,number2):
+  x = number1 + number2
+  return x
+def sub(number1,number2):
+  x = number1 - number2
+  return x
+def mul(number1,number2):
+  x = number1 * number2
+  return x
+def div(number1,number2):
+  x = number1 / number2
+  return x
+
+
+
+
+print(add(1,2))
